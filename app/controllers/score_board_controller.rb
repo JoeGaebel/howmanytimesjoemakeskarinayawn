4c52934fabc196fcd96d2e4f841c880363986c03
JoeGaebel <joe.gaebel@hotmail.com>
2017-03-12 01:46:39 -0500

    fix auth

--- app/controllers/score_board_controller.rb
+++ app/controllers/score_board_controller.rb
@@ -1,5 +1,6 @@
 class ScoreBoardController < ApplicationController
   TOKEN = "4c91feb4c123a608138e9bb7eb35f712b862e8a06fb0b3aa9fef4ba7cc5114b1"
+  before_action :authenticate_user, only: [:score]
 
   def index
     refresh_counts
@@ -7,12 +8,7 @@ class ScoreBoardController < ApplicationController
 
   def score
     if yawn_params[:increment] == "true"
-      if authenticated?
-        Yawn.create(is_joe: yawn_params[:is_joe])
-      else
-        flash[:error] = "Woah, who dis?"
-        redirect_to root_path
-      end
+      Yawn.create(is_joe: yawn_params[:is_joe])
     end
 
     refresh_counts
@@ -32,9 +28,18 @@ class ScoreBoardController < ApplicationController
 
   private
 
-  def authenticated?
-    authenticate_or_request_with_http_basic do |_, password|
-      return ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), TOKEN)
+  def authenticate_user
+    if yawn_params[:increment] == "true"
+      authenticate_or_request_with_http_basic('Administration') do |_, password|
+        auth = ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), TOKEN)
+        unless auth
+          flash[:error] = "Woah, who dis?"
+          redirect_to root_path
+          return
+        end
+
+        auth
+      end
     end
   end
 end
